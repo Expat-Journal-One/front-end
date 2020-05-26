c1ebae683fd5031eadd1d1dda4f7f833d9a0b0be
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-05-26 01:27:36 -0400

    changed the folders to be simpler

---
+++ app/src/Components/UserPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+class UserPage extends React.Component {
+    render() {
+        return (
+          <div>
+            <h1>User Page!</h1>
+            </div>
+    );
+  }
+}
+
+export default UserPage;
